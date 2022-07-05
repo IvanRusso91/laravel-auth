fc1bba19ecd39e3ecbc7e2139656fc78cedb26eb
IvanRusso91 <iv.russo91@gmail.com>
2022-07-05 16:43:47 +0200

    inserito db e popolato

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use illuminate\Support\Str;
 
 class Post extends Model
 {
-    //
+    public static function generateSlug($title){
+
+        $slug = Str::slug($title, '-');
+        $slug_base= $slug;
+
+        $post_slug= Post::where('slug', $slug)->first();
+        $c=1;
+
+        while($post_slug){
+            $slug = $slug_base .'-'. $c;
+            $c++;
+            $post_presente= Post::where('slug', $slug)->first();
+        }
+        return $slug;
+    }
 }
--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
 
 class PostsTableSeeder extends Seeder
 {
@@ -9,8 +11,14 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i < 10; $i++) {
+            $new_post=new Post();
+            $new_post-> title =  $faker ->company();
+            $new_post-> slug= Post::generateSlug($new_post->title);
+            $new_post->content= $faker ->text();
+            $new_post->save();
+        }
     }
 }
